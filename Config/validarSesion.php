f09efa48699bb7de0f1941d81d2a352f9eb4f140
Brayan <bfsanchez45@misena.edu.co>
2024-06-08 18:15:53 -0500

    inactividad 5 mn

--- Config/validarSesion.php
+++ Config/validarSesion.php
@@ -11,5 +11,17 @@ if (isset($_GET['logout'])) {
     header("Location: ../../../../crm/index.php");
     exit;
 }
+$tiempo_inactividad = 300; // 5 minutos
 
+// Comprueba si la sesión ha expirado debido a la inactividad
+if (isset($_SESSION['tiempo_ultimo_acceso']) && (time() - $_SESSION['tiempo_ultimo_acceso']) > $tiempo_inactividad) {
+    // Destruye la sesión y redirige a la página de inicio de sesión
+    session_unset();
+    session_destroy();
+    header("Location: ../../../../crm/index.php");
+    exit;
+}
+
+// Actualiza el tiempo del último acceso
+$_SESSION['tiempo_ultimo_acceso'] = time();
 ?>
